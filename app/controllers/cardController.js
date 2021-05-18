7570ccc27c138b1c0ab7bd7d93aa537aa7ae4581
cazerta <marcelo.cazerta@gmail.com>
2021-05-18 20:48:20 -0300

    Versão 1 - com banco de palavras/validação

--- app/controllers/cardController.js
+++ app/controllers/cardController.js
@@ -1,12 +1,27 @@
 var app = angular.module('axisMundi', []);
+responsiveVoice.setDefaultVoice("Brazilian Portuguese Female");
 
 app.controller('gramaticaCtrl', function($scope, $http) {
     let ctrl = this;
     ctrl.palavras = [];
     $http
-        .get('https://raw.githubusercontent.com/cazerta/AxisMundiHelper/main/files/bancoPalavras.json?token=ABOMWIGGHHLYEPMDM3TGSXLAUPSIM')
+        .get('https://raw.githubusercontent.com/cazerta/AxisMundiHelper/main/files/bancoPalavras.json')
         .then((data) => {
             console.log(data);
             ctrl.palavras = data.data.banco;
         });
+    
+    ctrl.speakWord = function(word) {
+        responsiveVoice.speak(word);
+    }
+
+    ctrl.validateWord = function(wordObject) {
+        let upperCaseTyped = wordObject.digitada.toUpperCase();
+        if (wordObject.palavra === upperCaseTyped) {
+            wordObject.valid = true;
+            wordObject.locked = true;
+        } else {
+            wordObject.valid = false;
+        };
+    }
 })
